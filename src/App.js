3e7be1bfdd4324ae30b5b592ed5c21922fdbb59f
aditi2906 <aditi19csu011@ncuindia.edu>
2022-03-05 10:15:08 +0530

    scroll to top implemented

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 import "./App.css";
 import Homepage from "./containers/homepage";
 import Body from "./components/Body/Body";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return <div className="App">
+  
   <BrowserRouter>
+  <ScrollToTop/>
   <Routes>
+ 
   <Route path="/" element={<Homepage/>} />
   <Route path="/body" element={<Body/>} />
   </Routes>
